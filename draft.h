8f56536ce805c13d24ebbfb612bb107ea3f055e1
Jh700wolf <148020631+Jh700wolf@users.noreply.github.com>
2024-09-17 05:49:57 -0600

    Add files via upload

---
+++ draft.h
@@ -0,0 +1,30 @@
+#define DRAFT_H_
+#ifndef DRAFT_H_
+
+#include <string>
+#include <iostream>
+#include <sstream> //Para el uso de Strings
+
+using namespace std;
+
+class Draft
+{
+private:
+    sting nombre;
+    string *cartas[60];
+public:
+    Draft();
+    Draft(string);
+    ~Draft();
+    
+};
+
+draft::draft(/* args */)
+{
+}
+
+draft::~draft()
+{
+}
+
+#endif 
