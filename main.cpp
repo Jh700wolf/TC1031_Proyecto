92e1807e6d3265f16d906b6a3b884c2ea5ac0e70
Jh700wolf <148020631+Jh700wolf@users.noreply.github.com>
2024-09-17 05:41:42 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,9 @@
+/*
+ORGANIZADOR DE CARTAS DE MTG
+Autor: José Pablo Gonzalez
+Descripción:
+Este código va a servir para poder almacenar una lista de cartas obtenidas en un draft. 
+Se van a tener varios objetos de drafts, y cada uno va a tener un arreglo de costes y 
+otro arreglo de cartas.
+
+*/
